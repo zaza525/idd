1edf663558a7673978ef646f262d95721d41ec1f
Ziang Gao <zianggao@berkeley.edu>
2020-04-11 03:50:49 -0700

    finish lab03

--- Drummer/Drummer/Controller/DrummerViewController.swift
+++ Drummer/Drummer/Controller/DrummerViewController.swift
@@ -28,7 +28,17 @@ class DrummerViewController: UIViewController {
     
     
     // YOUR CODE HERE
-
+    @IBAction func drumButtonWasPressed(_ sender: UIButton) {
+        currentDrumKit?.playDrumSound(forDrumWithTag: sender.tag)
+    }
+    
+    @IBAction func drumKitWasChanged(_ sender: UISegmentedControl) {
+        if (sender.selectedSegmentIndex == 0) {
+            currentDrumKit = drumKit0
+        } else {
+            currentDrumKit = drumKit1
+        }
+    }
 
 }
 
